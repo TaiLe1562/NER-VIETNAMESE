9a2c611e51f04d9efa05189cb66d90d54648330c
TaiLe1562 <leanhtai1562000@gmail.com>
2021-12-08 14:48:26 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, render_template, request
+import model
+app = Flask(__name__)
+
+crf = model.CRF()
+
+
+@app.route('/get_ner', methods=['GET', 'POST'])
+def get_ner():
+    if request.method == "POST":
+        paragraph = request.form['text']
+        # print(paragraph)
+        entities, tokenizes = crf.get_entity(paragraph)
+        print(entities)
+        return jsonify(dict({'entities': entities, 'tokenizes': tokenizes}))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+app.run(host='0.0.0.0', port=8000, debug=True)
